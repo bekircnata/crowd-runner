aa202c8f6896725d2f4539eb1843c291ddeb090d
Bekir Can Ata <bekircnata@gmail.com>
2024-02-10 22:15:54 +0300

    Crowd counter eklendi

--- Assets/Crowd Runner/Scripts/CrowdSystem.cs
+++ Assets/Crowd Runner/Scripts/CrowdSystem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CrowdSystem : MonoBehaviour
 {
+    [SerializeField] private Transform runnersParent;
     [SerializeField] private float radius;
     [SerializeField] private float angle;
 
@@ -21,10 +22,10 @@ public class CrowdSystem : MonoBehaviour
 
     private void PlaceRunners()
     {
-        for (int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < runnersParent.childCount; i++)
         {
             Vector3 childLocalPosition = GetRunnerLocalPosition(i);
-            transform.GetChild(i).localPosition = childLocalPosition;
+            runnersParent.GetChild(i).localPosition = childLocalPosition;
         }
     }
 
