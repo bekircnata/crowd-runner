2ff4e41c0ec81194ab060addc06c4f231406bc6b
Bekir Can Ata <bekircnata@gmail.com>
2024-02-19 22:01:42 +0300

    Game Manager ve play buton ui eklendi

---
+++ Assets/Crowd Runner/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public enum GameState { Menu, Game, LevelComplete, Gameover };
+
+    private GameState gameState;
+
+    public static Action<GameState> onGameStateChanged;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }else
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetGameState(GameState gameState)
+    {
+        this.gameState = gameState;
+        onGameStateChanged?.Invoke(gameState);
+    }
+}
