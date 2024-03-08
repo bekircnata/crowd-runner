c58410be4a9f3e4eb2c70bfc6ac69a65b664ca3e
Bekir Can Ata <bekircnata@gmail.com>
2024-03-08 23:07:52 +0300

    Enemy group generate

---
+++ Assets/Crowd Runner/Scripts/EnemyGroup.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroup : MonoBehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] private Enemy enemyPrefab;
+    [SerializeField] private Transform enemiesParent;
+
+    [Header(" Settings ")]
+    [SerializeField] private int amount;
+    [SerializeField] private float radius;
+    [SerializeField] private float angle;
+
+    void Start()
+    {
+        GenerateEnemies();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void GenerateEnemies() 
+    {
+        for (int i = 0; i < amount; i++ )
+        {
+            Vector3 enemyLocalPosition = GetEnemyLocalPosition(i);
+
+            Vector3 enemyWorldPoaition = enemiesParent.TransformPoint(enemyLocalPosition);
+
+            Instantiate(enemyPrefab, enemyWorldPoaition, Quaternion.identity, enemiesParent);
+        }
+    }
+
+    private Vector3 GetEnemyLocalPosition(int index)
+    {
+        float x = radius * Mathf.Sqrt(index) * Mathf.Cos(Mathf.Deg2Rad * index * angle);
+        float z = radius * Mathf.Sqrt(index) * Mathf.Sin(Mathf.Deg2Rad * index * angle);
+
+        return new Vector3(x, 0, z);
+    }
+}
